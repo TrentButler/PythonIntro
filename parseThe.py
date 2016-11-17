2b18026a0811433d7844f1f418433e7112885ae6
Trent Butler <redtrentbutler@gmail.com>
2016-11-17 12:57:15 -0600

    Python 11-17-16

---
+++ parseThe.py
@@ -0,0 +1,28 @@
+import os
+import urllib2
+import fileinput
+os.system('color 0a') #app color
+
+
+
+
+# Display
+print("This program looks for: \n")
+print("- How many times 'the' is used in a webpage")
+print("- What line it was used in")
+print("\n \n")
+print("Input Webpage....")
+iNput = raw_input()
+
+
+#get webpage from user input
+
+#pull info from webpage/ export to text file
+
+
+#look for 'the' in the file
+
+
+#export what was found to 'result.txt'
+
+#os.startfile(result.txt) something like that.....
