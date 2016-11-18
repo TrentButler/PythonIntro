4559cb4b9105bed8e19dd488f8275edd003d714d
Trent Butler <redtrentbutler@gmail.com>
2016-11-17 20:45:44 -0600

    Python 11-17-16

--- parseThe.py
+++ parseThe.py
@@ -1,8 +1,12 @@
 import os
 import urllib2
 import fileinput
+import json
 os.system('color 0a') #app color
 
+eXport = open('export.txt', 'w+')
+result = open('result.txt', 'w+')
+cOunt = 0
 
 
 
@@ -11,18 +15,48 @@ print("This program looks for: \n")
 print("- How many times 'the' is used in a webpage")
 print("- What line it was used in")
 print("\n \n")
-print("Input Webpage....")
-iNput = raw_input()
+
+#user input
+iNput = raw_input("Input Webpage ~>> ")
 
 
 #get webpage from user input
+page = urllib2.urlopen(iNput)
 
 #pull info from webpage/ export to text file
+eXport.write(page.read()) 
 
 
 #look for 'the' in the file
+for line in fileinput.input('export.txt'):
+	if ' the ' in line:
+		cOunt +=1		
+		#result.write("count: " count)
+		result.write("string found was: \n" + line)
+		result.write("\n")
+		#I need a more specific if statement?
+	
+	if ' Redtrent ' in line:
+		cOunt +=1
+		result.write("string found was: \n" + line)
+		result.write("\n")
+		#USED WHEN PARSING MY FACEBOOK PAGE....
+		
+
+os.startfile('result.txt')
+print('count:' , cOunt)
+raw_input()
+
+#help = [{'count:' cOunt, 'string found:' line}]
+#parsed_json = json.loads(help)
+#result.write(json.dumps(parsed_json)
+
+
+
+
 
 
 #export what was found to 'result.txt'
 
 #os.startfile(result.txt) something like that.....
+#os.startfile('C:\\Users\\trent.butler\\Desktop\\Python Intro\\result.txt')
