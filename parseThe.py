e26c9364944a410aea9689e0747439fbe17b9130
Trent Butler <redtrentbutler@gmail.com>
2016-11-17 20:41:29 -0600

    Revert "Python 11-17-16"
    This reverts commit 6ab22ea02f81a67e71b389dd9b02fe8093ed09a5.

--- parseThe.py
+++ parseThe.py
@@ -1,12 +1,8 @@
 import os
 import urllib2
 import fileinput
-import json
 os.system('color 0a') #app color
 
-eXport = open('export.txt', 'w+')
-result = open('result.txt', 'w+')
-cOunt = 0
 
 
 
@@ -15,40 +11,18 @@ print("This program looks for: \n")
 print("- How many times 'the' is used in a webpage")
 print("- What line it was used in")
 print("\n \n")
-
-#user input
-iNput = raw_input("Input Webpage ~>> ")
+print("Input Webpage....")
+iNput = raw_input()
 
 
 #get webpage from user input
-page = urllib2.urlopen(iNput)
 
 #pull info from webpage/ export to text file
-eXport.write(page.read()) 
 
 
 #look for 'the' in the file
-for line in fileinput.input('export.txt'):
-	if ' the ' in line:
-		cOunt + 1
-		
-		#result.write("count: " count)
-		result.write("string found was: " + line)
-		
-
-os.startfile('C:\\Users\\trent.butler\\Desktop\\Python Intro\\result.txt')
-raw_input()
-
-#help = [{'count:' cOunt, 'string found:' line}]
-#parsed_json = json.loads(help)
-#result.write(json.dumps(parsed_json)
-
-
-
-
 
 
 #export what was found to 'result.txt'
 
 #os.startfile(result.txt) something like that.....
-#os.startfile('C:\\Users\\trent.butler\\Desktop\\Python Intro\\result.txt')
