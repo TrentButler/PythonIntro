8e341a39b030f8fc62437f93e603af87d5c09161
Trent Butler <redtrentbutler@gmail.com>
2017-04-05 12:53:01 -0500

    Astar 04-05-17

--- ASTAR/AStar.py
+++ ASTAR/AStar.py
@@ -1,148 +1,76 @@
-from AstarTest import UnitTest
-import AstarTest
-from vector import Vector2
-
+from _test import *
 # WHAT I NEED
 
-# GetPosition()
-def GetPosition(node):
-    return (node.position.xpos, node.position.ypos)
 
-# COMPARETO
-def compareTo(one, positionKey):
-    if GetPosition(one) == positionKey:
-        if one.iswalkable is True:
-            return True
-    return False
 # GetDistance()
 def GetDistance(one, two):
-    xDist = abs(one.position.xpos - two.position.xpos) * 10
-    yDist = abs(one.position.ypos - two.position.ypos) * 10
+    xDist = abs(two[0] - one[0])
+    yDist = abs(two[1] - one[1])
     return xDist + yDist
 # GetAdjacentList()
-def GetAdjacentList(self, ID, grid):
-    adjacentList = []
-    adjacents = []
-    node = grid.getnode(ID)
+def GetAdjacentList(node, grid):
+    return getneighbors(node, grid)
 
-    a_adjacentRight = (node.position.xpos + distance, node.position.ypos)
-    b_diagUpRight = (node.position.xpos + distance, node.position.ypos - distance)
-    c_adjacentUp = (node.position.xpos, node.position.ypos - distance)
-    d_diagUpLeft = (node.position.xpos - distance, node.position.ypos - distance)
-    e_adjacentLeft = (node.position.xpos - distance, node.position.ypos)
-    f_diagDwnLeft = (node.position.xpos - distance, node.position.ypos + distance)
-    g_adjacentDown = (node.position.xpos, node.position.ypos + distance)
-    h_diagDwnRight = (node.position.xpos + distance, node.position.ypos + distance)
-
-    for node in self.grid.nodes:
-        if compareTo(GetPosition(node), d_diagUpLeft) is True:
-            # x.UpdateNode(self.diagMovement, x.GetDistance(target))
-            # x.SetParent(node)
-            adjacents.append(("d", node))
-            # adjacentList.append(x)
-        if compareTo(GetPosition(node), b_diagUpRight) is True:
-            # x.UpdateNode(self.diagMovement, x.GetDistance(target))
-            # x.SetParent(node)
-            adjacents.append(("b", node))
-            # adjacentList.append(x)
-        if compareTo(GetPosition(node), f_diagDwnLeft) is True:
-            # x.UpdateNode(self.diagMovement, x.GetDistance(target))
-            # x.SetParent(node)
-            adjacents.append(("f", node))
-            # adjacentList.append(x)
-        if compareTo(GetPosition(node), h_diagDwnRight) is True:
-            # x.UpdateNode(self.diagMovement, x.GetDistance(target))
-            # x.SetParent(node)
-            adjacents.append(("h", node))
-            # adjacentList.append(x)
-        if x.compareTo(GetPosition(node), c_adjacentUp) is True:
-            # x.UpdateNode(self.regMovement, x.GetDistance(target))
-            # x.SetParent(node)
-            adjacents.append(("c", node))
-            # adjacentList.append(x)
-        if x.compareTo(GetPosition(node), g_adjacentDown) is True:
-            # x.UpdateNode(self.regMovement, x.GetDistance(target))
-            # x.SetParent(node)
-            adjacents.append(("g", node))
-            # adjacentList.append(x)
-        if x.compareTo(GetPosition(node), e_adjacentLeft) is True:
-            # x.UpdateNode(self.regMovement, x.GetDistance(target))
-            # x.SetParent(node)
-            adjacents.append(("e", node))
-            # adjacentList.append(x)
-        if x.compareTo(GetPosition(node), a_adjacentRight) is True:
-            # x.UpdateNode(self.regMovement, x.GetDistance(target))
-            # x.SetParent(node)
-            adjacents.append(("a", node))
-            # adjacentList.append(x)
-
-    # for node in adjacentDict:
-    adjacents.sort(key=lambda x: x[0])
-    for node in adjacents:
-        adjacentList.append(node[1])
-
-    return adjacentList
-# SetParent()
-def SetParent(node, parentNode):
-    node.parent = parentNode
-# RETRACE()
 def Retrace(end):
     retraced = []
     currentNode = end
     while currentNode.parent is not None:
         retraced.append(currentNode)
         currentNode = currentNode.parent
+    # retraced.append(currentNode)
     return retraced
 
-# UpdateNode()
-def UpdateNode(node, G, H):
-    node.gcost = G
-    node.hcost = H
-    node.fcost = node.gcost + node.hcost
-
+def MoveCost(node, other):
+    if node[0] == other[0] or node[1] == other[1]:
+        return 10
+    return 14
 
 # ALGORITHUM 
 
 def AStarAlgorithum(start, goal, environment):  # PARENTING NEEDS WORK
-        openlist = []
-        closedlist = []
-        current = start
-        target = goal        
+    openlist = []
+    closedlist = []
+    result = []
+    current = start
+    target = goal
+    
+    # UpdateNode(current, current.g, GetDistance(current, target))
+    current.g = 0
+    current.h = GetDistance(current, target)
+    current.f = current.g + current.h
+    openlist.append(current)
+
+    while len(openlist) is not 0:
+
+        openlist.sort(key=lambda x: x.f)  # SORT OPENLIST
+        current = openlist[0]  # ASSIGN CURRENTNODE MOST OPTIMAL NODE IN OPENLIST
+        openlist.remove(current)  # REMOVE CURRENTNODE FROM OPENLIST
+        closedlist.append(current)  # APPEND CURRENTNODE TO CLOSED LIST            
+        if current is target:  # CHECK IF CURRENTNODE IS TARGETNODE
+            result = Retrace(current)
+            break
         
-        UpdateNode(current.gcost, GetDistance(current, target))
-        openlist.append(current)
-
-        while len(openlist) is not 0:
-
-            openlist.sort(key=lambda x: x.fcost)  # SORT OPENLIST
-            current = openlist[0]  # ASSIGN CURRENTNODE MOST OPTIMAL NODE IN OPENLIST
-
-            if current is target:  # CHECK IF CURRENTNODE IS TARGETNODE
-                Retrace(goal)
-                break
+        adjList = GetAdjacentList(current, environment)  # GET CURRENTNODE'S ADJACENTS
+        
+        for node in adjList:  # CHECK ALL ADJACENT NODES FOR BEST PATH
+            if node in closedlist or node.walkable is False:
+                continue
             
-            openlist.remove(current)  # REMOVE CURRENTNODE FROM OPENLIST
-            closedlist.append(current)  # APPEND CURRENTNODE TO CLOSED LIST
-            adjList = GetAdjacentList(current.value, 1)  # GET CURRENTNODE'S ADJACENTS
+            tgCost = node.g + MoveCost(current, node)  # needs work
 
-            for node in adjList:  # CHECK ALL ADJACENT NODES FOR BEST PATH
-                if node in closedlist:
-                    continue
+            if node not in openlist and node.walkable is True:
+                node.parent = current
+                node.g = MoveCost(current, node)
+                node.h = GetDistance(node, target)
+                node.f = node.g + node.h
+                openlist.append(node)
 
-                tgCost = current.gcost + node.gcost  # needs work
+            if tgCost >= node.g:
+                continue
 
-                if node not in openlist and node.iswalkable is True:
-                    SetParent(node, current)                    
-                    openlist.append(node)
+            node.parent = current
+            node.g = tgCost
+            node.h = GetDistance(node, target)
+            node.f = node.g + node.h
 
-                if tgCost >= node.gcost:
-                    continue
-
-                SetParent(node, current)
-                node.gcost = tgCost
-                node.hcost = GetDistance(node, target)
-                node.fcost = node.gcost + node.hcost
-
-
-test = UnitTest("test.txt")
-testing = test.testastar(AStarAlgorithum)
+    return result
---
+++ ASTAR/test.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.insert(0, "C:\Users\Redtrent\Documents\GitHub\PythonIntro")
+from AStar import *
+from _test import *
+ 
+#to test your astar it must follow these conventions
+#preconditions: node objects must have g f h and parent variables
+#node objects must access position elements through node[0] "posx" or node[1] "posy"
+#so if you have a node.positionx it will be node[0]
+#so if you have a node.positiony it will be node[1]
+#to calculate neighbors replace your fetch for adjacents/neighbors with
+#getneighbors(current, graph)
+#this returns a list of nodes
+#parameters: start, goal, graph
+#postconditions: function will return a list
+
+
+def main():
+    failcount = 0
+    passcount = 0
+    for _ in range(100):
+        res = testfunc(AStarAlgorithum)
+        if res:
+            passcount += 1
+        else:
+            failcount += 1
+    print str.format('fails {0}, passes {1}', failcount, passcount)
+    
+
+if __name__ == '__main__':
+    main()
