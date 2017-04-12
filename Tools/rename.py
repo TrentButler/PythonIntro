1bfdae893f78e52163625120925e42c44d95e30f
Trent Butler <redtrentbutler@gmail.com>
2017-04-12 13:43:17 -0500

    Rename 04-12-17

--- Tools/rename.py
+++ Tools/rename.py
@@ -8,7 +8,7 @@ for thing in current_dir:
     if thing.endswith('.py'):
         continue
     extension = os.path.splitext(thing)[1]
-    os.rename(thing, str(counter) + "_" +  _input + extension)
+    os.rename(thing, _input + "_" + str(counter) +  extension)
     counter += 1
     
 print 'SUCCESS'
